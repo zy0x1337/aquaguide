311155b4339a10dc2990ac716f7c704b99ed2049
zy0x1337 <zy0x@web.de>
2026-02-16 14:51:03 +0100

    feat: add auth page and wrap app with AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { ComparisonProvider } from './contexts/ComparisonContext';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import './index.css';
 
@@ -10,9 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <ComparisonProvider>
-          <App />
-        </ComparisonProvider>
+        <AuthProvider>
+          <ComparisonProvider>
+            <App />
+          </ComparisonProvider>
+        </AuthProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
