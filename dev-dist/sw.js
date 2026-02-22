69ef2826e99ab2c36603c01a869684607297955d
zy0x1337 <zy0x@web.de>
2026-02-22 01:03:49 +0100

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tv0prs9hthg"
+    "revision": "0.t6j35vsjlp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
