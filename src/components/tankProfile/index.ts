f9680212497829e9579e3b3fada57726d8e9560b
zy0x1337 <zy0x@web.de>
2026-02-25 21:25:19 +0100

    docs: Tank Profile Feature Dokumentation & Exports
    - index.ts für saubere Component-Imports
    - README.md mit Feature-Übersicht und Verwendungsbeispielen
    - API Integration Guidelines
    - Datenstruktur-Übersicht

---
+++ src/components/tankProfile/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Tank Profile Components
+ * Export barrel for easy imports
+ */
+
+export { TankProfileCard } from './TankProfileCard';
+export { TankProfileGallery } from './TankProfileGallery';
+export { CreateTankProfileModal } from './CreateTankProfileModal';
+export { TankImageUpload } from './TankImageUpload';
