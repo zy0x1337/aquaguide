110538275bbcd3245d000ae52318ab6a4d82de41
zy0x1337 <zy0x@web.de>
2026-02-25 21:29:04 +0100

    revert: Tank Profile Feature entfernt - wird später implementiert
    Zurück auf Stand nach FavoriteButton Update (682bcda)
    - tankProfile.ts Types gelöscht
    - Alle tankProfile Komponenten entfernt
    - README und Docs entfernt

--- src/components/tankProfile/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Tank Profile Components
- * Export barrel for easy imports
- */
-
-export { TankProfileCard } from './TankProfileCard';
-export { TankProfileGallery } from './TankProfileGallery';
-export { CreateTankProfileModal } from './CreateTankProfileModal';
-export { TankImageUpload } from './TankImageUpload';
